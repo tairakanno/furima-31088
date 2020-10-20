599ab826898fd060d8d4299f80a549affceab2e9
tairakanno <tairakannotaira@gmail.com>
2020-10-20 17:18:37 +0900

    新規登録画面実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_katakana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :birth_year_id, presence: true
+  validates :birth_month_id, presence: true
+  validates :birth_day_id, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :birth_year
+  belongs_to_active_hash :birth_month
+  belongs_to_active_hash :birth_day
 end
