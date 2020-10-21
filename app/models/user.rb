755a9d7ee061b977e459b203a76adb2403687b42
tairakanno <tairakannotaira@gmail.com>
2020-10-21 17:24:24 +0900

    本名は全角のバリデーションをつける修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
     validates :birth_date
   end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_katakana
     validates :first_name_katakana
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
