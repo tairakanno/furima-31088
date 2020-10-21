a37a90a2ed552038078dd00b135e9aa58e0c8000
tairakanno <tairakannotaira@gmail.com>
2020-10-21 20:08:55 +0900

    itemsテーブル実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+
+  validates :title, :text, :genre, presence: true
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
+ 
 end
