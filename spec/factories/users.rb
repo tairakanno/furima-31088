da64b4e13bd5201eac7c714621b068407d5b0954
tairakanno <tairakannotaira@gmail.com>
2020-10-21 12:22:14 +0900

    テストコード準備

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker: :Name.last_name}
+    email {Faker: :Internet.free_email}
+    password {Faker: :Internet.password(min_length:6)}
+    password_confirmation {password}
+    last_name {Faker: :name.last_name}
+    first_name {Faker: :name.last_name}
+    last_name_katakana  {"カタ"}
+    first_name_katakana  {"カナ"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,50 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
+  
+describe "ユーザー管理機能について" do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+context "新規登録できる時" do
+it "ユーザー情報が正しく入力できてる" do
+  expect(@user).to be_valid
+end
+end
+context "新規登録できない時"　do
+  it "nicknameが空の時"　do
+    @user.nickname = ""
+    @user.valid?
+    binding.pry
+  end
+  it "emailが空の時"　do
+  end
+  it "emailが他と被っている時"　do
+  end
+  it "passwordが空の時"　do
+  end
+  it "password_confirmationが空のとき" do
+  end
+  it "passwordとpassword_confirmationが違う時" do
+  end
+  it "passwordが英語だけの時" do
+  end
+  it "passwordが数字だけの時" do
+  end
+  it "last_nameが空の時" do
+  end
+  it "first_nameが空の時" do
+  end
+  it "last_name_katakanaが空の時" do
+  end
+  it "first_name_katakanaが空の時" do
+  end
+  it "last_name_katakanaがカタカナ以外の時" do
+  end
+  it "first_name_katakanaがカタカナ以外の時" do
+  end
+end
+
+
+end
 end
