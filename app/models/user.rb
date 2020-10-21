a1877def6fb21d2e9ba796e53992bb155c0957c6
tairakanno <tairakannotaira@gmail.com>
2020-10-21 15:54:19 +0900

    passwordを英数を含めるバリテーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,13 @@ class User < ApplicationRecord
     validates :nickname
     validates :last_name
     validates :first_name
-    
     validates :birth_date
   end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
   validates :last_name_katakana
   validates :first_name_katakana
   end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   
 end
