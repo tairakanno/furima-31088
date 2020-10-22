7a9d4682004457804026390f1fda766a603f11e3
tairakanno <tairakannotaira@gmail.com>
2020-10-22 15:16:40 +0900

    バリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
-  # belongs_to :user
-  # has_one_attached :image
+  belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
 
-  # validates :title, :text, :genre, presence: true
+  validates :title, :text, :price, presence: true
 
-  # with_options presence: true, numericality: { other_than: 1 } do
-  #   validates :category_id
-  #   validates :status_id
-  #   validates :delivery_cost_id
-  #   validates :prefecture_id
-  #   validates :days_to_ship_id
-  # end
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
 end
