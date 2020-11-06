867bcc78ea84d0aedc302f4d95f6859549c98cd4
tairakanno <tairakannotaira@gmail.com>
2020-11-06 15:51:04 +0900

    “修正”

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -16,7 +16,6 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
@@ -25,7 +24,6 @@ const pay = () => {
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
-
       document.getElementById("charge-form").submit();
     });
   });
